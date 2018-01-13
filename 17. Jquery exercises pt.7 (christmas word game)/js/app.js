3b6b66d6922112232e4f26c603378216fc265adc
Bram Pauwelyn <brampauw@gmail.com>
2018-01-13 21:32:45 +0100

    add jquery slider - jquery plugin datatables - jquery plugin superslider

--- 17. Jquery exercises pt.7 (christmas word game)/js/app.js
+++ 17. Jquery exercises pt.7 (christmas word game)/js/app.js
@@ -1,16 +1,46 @@
 var wordList = ["rendier"];
 var splittedWord = wordList[0].split('');
+var score = 0;
+var guessedCharacters = 0;
 
-for(var i = 0; i < splittedWord.length; i++){
-  $('.word-placeholder').append('<div class="character"></div>');
-  $('.word-placeholder').css('width', splittedWord.length * 64 + 'px');
+function startGame(){
+  $('.word-placeholder').empty();
+  for(var i = 0; i < splittedWord.length; i++){
+    $('.word-placeholder').append('<div id="character'+i+'" class="character"></div>');
+    $('.word-placeholder').css('width', splittedWord.length * 64 + 'px');
+  }
 }
 
-$('button').on('click', function(){
-  var userInput = $('input').val();
-  for (var i = 0; i < splittedWord.length; i++) {
-    if(userInput === splittedWord[i]){
-      console.log('true');
+function checkCharacter(){
+  $('.submit-character').on('click', function(){
+    var userInput = $('input').val();
+    for (var i = 0; i < splittedWord.length; i++) {
+      if(userInput.toLowerCase() === splittedWord[i]){
+        $("#character"+i).html('<p>'+userInput+'</p>');
+        score += 10;
+        guessedCharacters++;
+        $('.score').text(score);
+      }
     }
-  }
-});
+    console.log(guessedCharacters);
+    if(guessedCharacters === splittedWord.length){
+      console.log('you won');
+    }
+  });
+}
+
+
+
+function resetGame(){
+  $('.reset').on('click', function(){
+    score = 0;
+    guessedCharacters = 0;
+    $('.score').text(score);
+    $('.input-character').val('');
+    startGame();
+  });
+}
+
+startGame();
+checkCharacter();
+resetGame();
--- 18. Jquery slider/js/app.js
+++ 18. Jquery slider/js/app.js
@@ -1,6 +1,48 @@
 var imageCount = $('.slider ul li').length;
 var imageWidth = $('.slider ul li').width();
 var currentImage = 1;
+var circleElement = '<div class="circle"></div>';
+
+
+
+  setInterval(function(){
+    autoSlide();
+  }, 3000);
+
+
+for (var i = 0; i < imageCount; i++) {
+  console.log(i);
+  $('.circle-container').append(circleElement);
+}
+
+$('.circle').eq(0).addClass('active');
+
+// setInterval(function(){
+//   _autoMove();
+// }, 1000);
+//
+// function _autoMove(){
+//   if(currentImage === imageCount){
+//     currentImage = 0;
+//   }
+//   var pxValue = -(currentImage * imageWidth);
+//   currentImage++;
+//   $('.slider ul').css('left', pxValue);
+//   $('.circle').removeClass('active');
+//   $('.circle:eq('+ (currentImage-1) +')').addClass('active');
+// }
+
+function autoSlide(){
+  if(currentImage === imageCount){
+    currentImage = 0;
+  }
+  var pxValue = -(currentImage * imageWidth);
+  currentImage++;
+  $('.slider ul').css('left', pxValue);
+  $('.circle').removeClass('active');
+  console.log(currentImage);
+  $('.circle:eq('+ (currentImage-1) +')').addClass('active');
+}
 
 $('.js-toggle').on('click', function(event){
   event.preventDefault();
@@ -11,7 +53,6 @@ $('.js-toggle').on('click', function(event){
       currentImage = 0;
     }
     var pxValue = -(currentImage * imageWidth);
-    $('.slider ul').css('left', pxValue);
     currentImage++;
   }else if (toggleType === 'prev') {
     currentImage--;
@@ -19,6 +60,9 @@ $('.js-toggle').on('click', function(event){
       currentImage = imageCount ;
     }
     var pxValue = -(currentImage-1) * imageWidth;
-    $('.slider ul').css('left', pxValue);
   }
+    $('.slider ul').css('left', pxValue);
+    $('.circle').removeClass('active');
+    console.log(currentImage);
+    $('.circle:eq('+ (currentImage-1) +')').addClass('active');
 });
